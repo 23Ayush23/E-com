46e22ff7d5aebf5f781df49e3467c20008ccd72e
Ayush <ayushdpatel2372003@gmail.com>
2025-03-11 14:33:00 +0530

    bug fix

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {port: 5174}
-  
+  server: {
+    port: process.env.PORT || 5174,
+    host: "0.0.0.0",
+    strictPort: true,
+    allowedHosts: ["https://e-com-1-glqf.onrender.com"], // Allow your deployed Render domain
+  }
 })
